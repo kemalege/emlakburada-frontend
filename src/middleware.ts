46c5ad3f994b39851c9faaf56b027e04d962fd14
kemalege <cirpanliege@gmail.com>
2024-07-20 01:14:55 +0300

    Create middleware

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const verify = req.cookies.get("authenticated")?.value === "true";
+
+  const url = req.url;
+
+    // if (!verify){
+    //     return NextResponse.redirect(
+    //         new URL('/login', req.url)
+    //     )
+    // }
+
+    if (!verify && url.includes('/myads')) {
+      return NextResponse.redirect("http://localhost:3000/login");
+    }
+  
+    // if (verify && url === "http://localhost:3000/") {
+    //   return NextResponse.redirect("http://localhost:3000/dashboard");
+
+    // // return NextResponse.next()
+    
+    // }
+}
