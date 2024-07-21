89b4601a326246560426f6623dfed95f7d5aff22
kemalege <cirpanliege@gmail.com>
2024-07-21 16:47:37 +0300

    Create purchase packet page

---
+++ src/app/(home)/ad-packets/[packageId]/purchase/page.tsx
@@ -0,0 +1,57 @@
+import PaymentPage from '@/components/PaymentPage';
+import apiFetch from '@/lib/api';
+import { TPaymentSchema } from '@/lib/validations/payment';
+import { AdPacket, PaymentDetails } from '@/types/api';
+import { cookies } from 'next/headers';
+import React from 'react'
+
+interface pageParams {
+    packageId: number;
+}
+
+interface PurchasePackageRequest {
+    userId: number;
+    packageId: number;
+    paymentDetails: {
+        cardNumber: string;
+        expiryDate: string;
+        cvv: number;
+    };
+}
+
+const fetchAdPackage = async (packageId: number) => {
+    const result = await apiFetch<AdPacket>(`/packages/${packageId}`,{
+      method: 'GET',
+    } );
+    if (result.status === 'SUCCESS') {
+        return result.data;
+    } else {
+      console.error('Error from server:', result.error);
+    }
+  }
+
+  const purschasePacket = async (data: PurchasePackageRequest) => {
+    'use server'
+    const result = await apiFetch(`/packages}`,{
+      method: 'POST',
+      body: {
+        packageId: data.packageId,
+        userId: data.userId,
+        paymentDetails: data.paymentDetails,
+      }
+    } );
+    if (result.status === 'SUCCESS') {
+        return result.data;
+    } else {
+      console.error('Error from server:', result.error);
+    }
+  }
+
+export default async function PurschasePacketPage({params}: {params: pageParams}) {
+    
+    const data = await fetchAdPackage(params.packageId);
+    
+    return (
+        data && <PaymentPage<AdPacket> product={data} onSubmitPayment={purschasePacket} />
+    )
+}
